22e144f5a0807f22cce79ab9a1112db9310396a0
Sourdeval <sourdeval@laposte.net>
2020-11-28 17:42:28 +0100

    3e Commit (peut etre 2e)

--- ChaineRespo/src/respochain/BrutalForceHandler.java
+++ ChaineRespo/src/respochain/BrutalForceHandler.java
@@ -2,4 +2,16 @@ package respochain;
 
 public class BrutalForceHandler implements IHandler{
 
+	@Override
+	public void setNext(IHandler h) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handle(String id, String pass) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
